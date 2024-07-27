3b7e2985ac4c85911c2c0bb21058ea17e7cf6073
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-07-27 14:04:53 -0700

    Added ModKit Property: Included an int property for ModKit with the [XmlElement("ModKit")] attribute to match the XML element.

--- Models/VehicleData.cs
+++ Models/VehicleData.cs
@@ -6,9 +6,15 @@ namespace ExtraManager.Models
     [XmlRoot("Vehicle")]
     public class VehicleData
     {
+        // The name attribute specifies the vehicle's model name in the game.
         [XmlAttribute("name")]
         public string Name { get; set; }
 
+        // The ModKit element references the modkit ID associated with this vehicle. 
+        [XmlElement("ModKit")]
+        public int ModKit { get; set; }
+
+        // List of extras available for the vehicle.
         [XmlArray("Extras")]
         [XmlArrayItem("Extra")]
         public List<Extra> Extras { get; set; }
