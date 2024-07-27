1f83f09962b492ad7941d120fa06d0ad7e20bed7
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-07-27 14:47:36 -0700

    Cleanup code

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -18,10 +18,11 @@ namespace ExtraManager
 
         private static List<VehicleData> vehicleList = VehicleDataManager.LoadVehicles().VehicleList;
         private static HashSet<uint> processedVehicleHandles = new HashSet<uint>();
-
         private static ModkitSet modkitSet = VehicleDataManager.LoadModkits();
 
         #endregion
+
+        #region Plugin Lifecycle
 
         public static void Main()
         {
@@ -35,7 +36,9 @@ namespace ExtraManager
             }
         }
 
-        #region Initialization
+        #endregion
+
+        #region Dependency Check
 
         private static bool CheckDependencies()
         {
@@ -69,6 +72,10 @@ namespace ExtraManager
             }
         }
 
+        #endregion
+
+        #region Plugin Initialization
+
         private static void InitializePlugin()
         {
             GameFiber.StartNew(delegate
@@ -77,81 +84,52 @@ namespace ExtraManager
                 {
                     GameFiber.Yield();
                     GameFiber.Sleep(100);
-                    Vehicle[] allVehicles = World.GetAllVehicles();
-                    foreach (Vehicle vehicle in allVehicles)
-                    {
-                        GameFiber.Yield();
-                        if (vehicle.IsValid())
-                        {
-                            uint modelHash = vehicle.Model.Hash;
-                            IntPtr modelNamePtr = NativeFunction.CallByHash<IntPtr>(0xB215AAC32D25D019, modelHash);
-
-                            if (modelNamePtr != IntPtr.Zero)
-                            {
-                                string modelName = Marshal.PtrToStringAnsi(modelNamePtr).ToUpper();
-
-                                if (!string.IsNullOrEmpty(modelName))
-                                {
-                                    var vehicleData = vehicleList.FirstOrDefault(v => v.Name == modelName);
-                                    if (vehicleData != null)
-                                    {
-                                        uint vehicleHandle = vehicle.Handle;
-
-                                        if (processedVehicleHandles.Contains(vehicleHandle))
-                                        {
-                                            continue;
-                                        }
-
-                                        #region ModKit Configuration
-
-                                        // Check if modkit is specified for the vehicle
-                                        var modkitConfig = vehicleData.ModKit.HasValue
-                                            ? modkitSet.Modkits.FirstOrDefault(m => m.Id == vehicleData.ModKit.Value)
-                                            : null;
-
-                                        if (modkitConfig != null)
-                                        {
-                                            if (NativeFunction.CallByHash<int>(0x33F2E3FE70EAAE1D, vehicle) > 0) // GET_NUM_MOD_KITS
-                                            {
-                                                NativeFunction.CallByHash<int>(0x1F2AA07F00B3217A, vehicle, 0); // SET_VEHICLE_MOD_KIT
-                                            }
-                                            VehicleDataManager.ApplyVehicleCustomization(vehicle, modkitConfig);
-                                        }
-
-                                        #endregion
-
-                                        #region Extra Configuration
-
-                                        for (int i = 1; i <= 14; i++)
-                                        {
-                                            bool isExtraEnabled = NativeFunction.CallByHash<bool>(0xD2E6822DBFD6C8BD, vehicle, i);
-
-                                            // Check if the extra is in the XML configuration
-                                            var extraConfig = vehicleData.Extras.FirstOrDefault(ec => ec.Id == i);
-                                            bool shouldEnable = extraConfig != null ? extraConfig.Enabled : false;
-
-                                            // If the current state doesn't match the configuration, update it
-                                            if (isExtraEnabled != shouldEnable)
-                                            {
-                                                NativeFunction.CallByHash<int>(0x7EE3A3C5E4A40CC9, vehicle, i, !shouldEnable);
-                                            }
-                                        }
-
-                                        #endregion
-
-                                        processedVehicleHandles.Add(vehicleHandle);
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    ProcessVehicles();
                 }
             });
         }
 
+        private static void ProcessVehicles()
+        {
+            Vehicle[] allVehicles = World.GetAllVehicles();
+            foreach (Vehicle vehicle in allVehicles)
+            {
+                GameFiber.Yield();
+                if (vehicle.IsValid())
+                {
+                    uint modelHash = vehicle.Model.Hash;
+                    IntPtr modelNamePtr = NativeFunction.CallByHash<IntPtr>(0xB215AAC32D25D019, modelHash);
+
+                    if (modelNamePtr != IntPtr.Zero)
+                    {
+                        string modelName = Marshal.PtrToStringAnsi(modelNamePtr).ToUpper();
+
+                        if (!string.IsNullOrEmpty(modelName))
+                        {
+                            var vehicleData = vehicleList.FirstOrDefault(v => v.Name == modelName);
+                            if (vehicleData != null)
+                            {
+                                uint vehicleHandle = vehicle.Handle;
+
+                                if (processedVehicleHandles.Contains(vehicleHandle))
+                                {
+                                    continue;
+                                }
+
+                                VehicleDataManager.ConfigureModkit(vehicle, vehicleData, modkitSet);
+                                VehicleDataManager.ConfigureExtras(vehicle, vehicleData);
+
+                                processedVehicleHandles.Add(vehicleHandle);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         #endregion
 
-        #region Commands
+        #region Console Commands
 
         [ConsoleCommand]
         private static void Command_GetCurrentVehicleName()
--- Models/Managers/VehicleDataManager.cs
+++ Models/Managers/VehicleDataManager.cs
@@ -2,6 +2,7 @@
 using Rage.Native;
 using System;
 using System.IO;
+using System.Linq;
 using System.Xml.Serialization;
 
 namespace ExtraManager.Models
@@ -15,7 +16,7 @@ namespace ExtraManager.Models
 
         #endregion
 
-        #region Vehicle Data Methods
+        #region Vehicle Data Loading
 
         public static VehicleSet LoadVehicles()
         {
@@ -32,7 +33,7 @@ namespace ExtraManager.Models
 
         #endregion
 
-        #region Modkit Data Methods
+        #region Modkit Data Loading
 
         public static ModkitSet LoadModkits()
         {
@@ -49,13 +50,47 @@ namespace ExtraManager.Models
 
         #endregion
 
-        #region Customization Methods
+        #region Vehicle Customization
 
         public static void ApplyVehicleCustomization(Vehicle vehicle, Modkit data)
         {
             int tintValue = data.WindowTint.HasValue ? data.WindowTint.Value : 0;
 
             NativeFunction.CallByHash<int>(0x57C51E6BAD752696, vehicle, tintValue); // SET_VEHICLE_WINDOW_TINT
+        }
+
+        public static void ConfigureModkit(Vehicle vehicle, VehicleData vehicleData, ModkitSet modkitSet)
+        {
+            var modkitConfig = vehicleData.ModKit.HasValue
+                ? modkitSet.Modkits.FirstOrDefault(m => m.Id == vehicleData.ModKit.Value)
+                : null;
+
+            if (modkitConfig != null)
+            {
+                if (NativeFunction.CallByHash<int>(0x33F2E3FE70EAAE1D, vehicle) > 0) // GET_NUM_MOD_KITS
+                {
+                    NativeFunction.CallByHash<int>(0x1F2AA07F00B3217A, vehicle, 0); // SET_VEHICLE_MOD_KIT
+                }
+                ApplyVehicleCustomization(vehicle, modkitConfig);
+            }
+        }
+
+        public static void ConfigureExtras(Vehicle vehicle, VehicleData vehicleData)
+        {
+            for (int i = 1; i <= 14; i++)
+            {
+                bool isExtraEnabled = NativeFunction.CallByHash<bool>(0xD2E6822DBFD6C8BD, vehicle, i);
+
+                // Check if the extra is in the XML configuration
+                var extraConfig = vehicleData.Extras.FirstOrDefault(ec => ec.Id == i);
+                bool shouldEnable = extraConfig != null ? extraConfig.Enabled : false;
+
+                // If the current state doesn't match the configuration, update it
+                if (isExtraEnabled != shouldEnable)
+                {
+                    NativeFunction.CallByHash<int>(0x7EE3A3C5E4A40CC9, vehicle, i, !shouldEnable);
+                }
+            }
         }
 
         #endregion
