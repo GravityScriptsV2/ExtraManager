df17d3e78bc734aaa512d8b90d9aa74e0489ee16
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-07-27 14:11:12 -0700

    Updated `VehicleDataManager` with `LoadModkits` method to deserialize `VehicleModkitsConfig.xml`.

--- Models/Managers/VehicleDataManager.cs
+++ Models/Managers/VehicleDataManager.cs
@@ -6,19 +6,45 @@ namespace ExtraManager.Models
 {
     public class VehicleDataManager
     {
-        private static readonly string XmlFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "/plugins/ExtraManager", "VehicleExtrasConfig.xml");
+        #region File Paths
+
+        private static readonly string VehicleXmlFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "/plugins/ExtraManager", "VehicleExtrasConfig.xml");
+        private static readonly string ModkitsXmlFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "/plugins/ExtraManager", "VehicleModkitsConfig.xml");
+
+        #endregion
+
+        #region Vehicle Data Methods
 
         public static VehicleSet LoadVehicles()
         {
             XmlSerializer serializer = new XmlSerializer(typeof(VehicleSet));
             VehicleSet vehicles;
 
-            using (FileStream fs = new FileStream(XmlFilePath, FileMode.Open))
+            using (FileStream fs = new FileStream(VehicleXmlFilePath, FileMode.Open))
             {
                 vehicles = (VehicleSet)serializer.Deserialize(fs);
             }
 
             return vehicles;
         }
+
+        #endregion
+
+        #region Modkit Data Methods
+
+        public static ModkitSet LoadModkits()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(ModkitSet));
+            ModkitSet modkits;
+
+            using (FileStream fs = new FileStream(ModkitsXmlFilePath, FileMode.Open))
+            {
+                modkits = (ModkitSet)serializer.Deserialize(fs);
+            }
+
+            return modkits;
+        }
+
+        #endregion
     }
 }
