d0c1e17fc8f95647a219562b28143f6fd158ec66
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-07-27 12:28:12 -0700

    Update Library Requirment for Venoxity.Common

--- UtilityConstants.cs
+++ UtilityConstants.cs
@@ -6,7 +6,7 @@ namespace ExtraManager
     {
 		public static readonly List<(string Name, string Version)> Dependencies = new List<(string, string)>
 		{
-			("Venoxity.Common.dll", "1.0.0.0"),
+			("Venoxity.Common.dll", "1.0.3.0"),
 		};
 	}
 }
