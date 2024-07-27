0af7f492f4ee11a7735279b612bd3332eab61411
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-07-27 14:57:40 -0700

    Prep RNUI

--- UtilityConstants.cs
+++ UtilityConstants.cs
@@ -6,6 +6,7 @@ namespace ExtraManager
     {
 		public static readonly List<(string Name, string Version)> Dependencies = new List<(string, string)>
 		{
+			("RAGENativeUI.dll", "1.0.2.0"),
 			("Venoxity.Common.dll", "1.0.3.0"),
 		};
 	}
