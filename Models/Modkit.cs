5f6eac7493bb0f28ed0ced7568a3106e52800302
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-07-27 14:08:40 -0700

    Add models for vehicle modkits and update VehicleDataManager to load modkits

---
+++ Models/Modkit.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace ExtraManager.Models
+{
+    public class Modkit
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+        [XmlElement("WindowTint")]
+        public int WindowTint { get; set; }
+    }
+}
---
+++ Models/ModkitSet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ExtraManager.Models
+{
+    [XmlRoot("Modkits")]
+    public class ModkitSet
+    {
+        [XmlElement("Modkit")]
+        public List<Modkit> Modkits { get; set; }
+    }
+}
