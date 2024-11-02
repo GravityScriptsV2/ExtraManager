880b0e9ec756061b03b8513d2828b9ee51180b51
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-11-01 18:43:51 -0700

    Fix some menu issues

--- Engine/FrontendSystems/ConfigMenu.cs
+++ Engine/FrontendSystems/ConfigMenu.cs
@@ -1,53 +1,32 @@
-﻿using RAGENativeUI.Elements;
-
-namespace ExtraManager.Engine.FrontendSystems
+﻿namespace ExtraManager.Engine.FrontendSystems
 {
     internal static class ConfigMenu
     {
         #region Fields
-
-        private static MenuPool Pool;
+        public static MenuPool Pool;
         public static UIMenu MainMenu;
-        private static UIMenuNumericScrollerItem<int> windowTintSelector;
-
-        private static readonly Dictionary<int, string> TintLevelNames = new Dictionary<int, string>
-        {
-            { -1, "None" },
-            { 0, "Stock" },
-            { 1, "Pure Black" },
-            { 2, "Dark Smoke" },
-            { 3, "Light Smoke" },
-            { 4, "Limo" },
-            { 5, "Green" },
-        };
         #endregion
 
+        #region Setup
         public static void Setup()
         {
             Pool = new MenuPool();
             MainMenu = new UIMenu("ExtraManager", "MAIN MENU ~y~v1.0");
 
-            var playerVehicle = Game.LocalPlayer.Character.CurrentVehicle;
-
-            //windowTintSelector = new UIMenuNumericScrollerItem<int>("Window Tint Level", "Adjust the tint level for your vehicle's windows.", -1, 5, 1) { Value = NativeFunction.CallByHash<int>(0x0EE21293DAD47C95, playerVehicle) };
-            //windowTintSelector.Formatter = value =>
-            //           TintLevelNames.ContainsKey(value) ? TintLevelNames[value] : "Unknown";
-
-            //MainMenu.AddItem(windowTintSelector);
-
             Pool.Add(MainMenu);
-
             GameFiber.StartNew(ProcessMenus);
         }
+        #endregion
 
+        #region Menu Processing
         private static void ProcessMenus()
         {
             while (true)
             {
                 GameFiber.Yield();
-
                 Pool.ProcessMenus();
             }
         }
+        #endregion
     }
 }
---
+++ Engine/FrontendSystems/ConsoleCommands.cs
@@ -0,0 +1,23 @@
+﻿using RAGENativeUI.PauseMenu;
+
+namespace ExtraManager.Engine.FrontendSystems
+{
+    internal class ConsoleCommands : CommonPlugin
+    {
+        [ConsoleCommand("Open the ExtraManager configuration menu")]
+        internal static void OpenExMConfigMenu()
+        {
+            if (!UIMenu.IsAnyMenuVisible && !TabView.IsAnyPauseMenuVisible)
+            {
+                if (ClientPed.IsInAnyVehicle(false))
+                {
+                    ConfigMenu.MainMenu.Visible = true;
+                }
+                else
+                {
+                    Game.DisplaySubtitle("You must be in a vehicle to access this menu.");
+                }
+            }
+        }
+    }
+}
